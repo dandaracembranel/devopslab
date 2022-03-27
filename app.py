4806e090270f0b14bd2c836e68c03d8e0e905d29
dandaracembranel <102266669+dandaracembranel@users.noreply.github.com>
2022-03-27 11:40:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
+    return "Hello World - Dandara"
 
 if __name__ == '__main__':
     app.run()
